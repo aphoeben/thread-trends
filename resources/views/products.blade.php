f8d4db860818231f31ee5580882c1784afd66880
LanceEnr <lancemikaelo.enriquez@gmail.com>
2023-12-13 08:41:58 +0800

    fixes

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -25,7 +25,7 @@
         <div style="background-color: #212529;" class="rounded-lg shadow-lg relative product-card">
             <form action="{{ route('wishlist.add', $product->id) }}" method="POST">
                 @csrf
-                <button class="text-gray-500 hover:text-red-500 absolute top-0 right-0 m-2">
+                <button class="text-gray-500 hover:text-red-800 absolute top-2 right-2 m-2">
                     <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path
                             d="M12 21.35l-1.45-1.32C5.4 16.16 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 7.66-8.55 11.54L12 21.35z" />
--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -4,9 +4,16 @@
 <div class="container mx-auto px-4 py-6">
     <h1 class="text-4xl font-bold text-center w-full">My Wishlist</h1>
 
+
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         @foreach(Auth::user()->wishlist as $product)
         <div style="background-color: #212529;" class="rounded-lg shadow-lg relative product-card">
+            <form action="{{ route('wishlist.remove', $product->id) }}" method="POST">
+                @csrf
+                <button type="submit" class="absolute top-2 right-2 m-2" style="color: #8d0606;">
+                    <i class="fa fa-trash fa-lg"></i>
+                </button>
+            </form>
             <img src="{{ asset('storage/products/' . $product->image) }}" alt="{{ $product->name }}"
                 class="w-full h-64 object-cover ">
             <div class="p-4">
@@ -15,12 +22,14 @@
 
                 <div class="flex justify-between items-center mt-4">
                     <span style="color: #8d0606;" class="text-xl font-bold">₱{{ number_format($product->price) }}</span>
+
                     <div class="space-x-2">
-                        <form action="{{ route('wishlist.remove', $product->id) }}" method="POST">
+
+                        <form action="{{ route('addToCart', $product->id) }}" method="POST">
                             @csrf
                             <button type="submit"
                                 class="bg-red-800 hover:bg-red-900 active:bg-red-700 text-white px-4 py-2 rounded">
-                                Remove from Wishlist
+                                Add to Cart
                             </button>
                         </form>
                     </div>
